07c6b21641253d2480984a60713fd6695ea583c5
bymahiwork-prog <by.mahi.work@gmail.com>
2026-05-13 16:11:31 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,22 @@ const nextConfig = {
   images: {
     domains: ["admin.effortlessevents.in"],
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "effortlessevents.in",
+          },
+        ],
+        destination: "https://www.effortlessevents.in/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
