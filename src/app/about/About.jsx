a4b0164cadc910262ae8bf02c3b2f1252aee8a59
bymahiwork-prog <by.mahi.work@gmail.com>
2026-05-15 13:38:49 +0530

    Update About.jsx

--- src/app/about/About.jsx
+++ src/app/about/About.jsx
@@ -1,6 +1,5 @@
 import EffortlessHosting from "@/utils/about/EffortlessHosting";
 import EventSpacePage from "@/utils/about/EventSpacePage";
-import FeaturedSection from "@/utils/about/FeaturedSection";
 import OurTeamSection from "@/utils/about/OurTeamSection";
 import React from "react";
 
