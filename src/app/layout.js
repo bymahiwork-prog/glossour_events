5ac8a44a51bc62ed311de474f4a52fa97b49218d
bymahiwork-prog <by.mahi.work@gmail.com>
2026-06-04 15:34:35 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 // src/app/layout.js
+
 import "./globals.css";
 import Footer from "@/components/Footer";
 import NavbarWrapper from "@/components/NavbarWrapper";
@@ -7,6 +8,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import GoogleAds from "@/components/GoogleAds";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -95,9 +97,37 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      {/* Google Tag Manager */}
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];
+          w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;
+          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KGZKTPHK');
+        `}
+      </Script>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KGZKTPHK"
+            height="0"
+            width="0"
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
+          />
+        </noscript>
+
         {/* Structured Data */}
         <script
           type="application/ld+json"
@@ -113,8 +143,9 @@ export default function RootLayout({ children }) {
         <main>{children}</main>
 
         <Footer />
+
         <GoogleAnalytics />
-          <GoogleAds />
+        <GoogleAds />
       </body>
     </html>
   );
