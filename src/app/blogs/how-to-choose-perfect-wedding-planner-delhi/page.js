4b737f965ad9c48a2b37e559e5a4fdba5e1190ac
bymahiwork-prog <by.mahi.work@gmail.com>
2026-08-13 14:38:44 +0530

    Update page.js

--- src/app/blogs/how-to-choose-perfect-wedding-planner-delhi/page.js
+++ src/app/blogs/how-to-choose-perfect-wedding-planner-delhi/page.js
@@ -39,7 +39,7 @@ export default function BlogPage() {
       <section className="max-w-6xl mx-auto px-6 pt-32 pb-10">
 
         <img
-          src="/blogs/how-to-choose-perfect-wedding-planner-delhi-cover.png"
+          src="/blogs/how-to-choose-perfect-wedding-planner-delhi",
           alt="How to Choose the Perfect Wedding Planner in Delhi: A Complete Guide"
           className="w-full rounded-xl mb-10"
         />
